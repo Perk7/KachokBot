0ed534999dd4600e163c66e590c846658c71d452
Perk7 <62936958+Perk7@users.noreply.github.com>
2022-04-08 09:25:04 +0700

    Fd

--- database.py
+++ database.py
@@ -26,7 +26,8 @@ class BotDB:
     def is_user(self, id_user):
         stmt = "SELECT id FROM users WHERE id = (?)"
         args = (id_user,) 
-        return bool(self.conn.execute(stmt, args).fetchall()[0])
+        print(self.conn.execute(stmt, args).fetchall())
+        return bool(self.conn.execute(stmt, args).fetchall())
 
     def update_user(self, id_user, obj):
         stmt = "UPDATE users SET data = (?) WHERE id = (?)"
