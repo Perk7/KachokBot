5fd83a021748f0aafe5ce6369bc20bac7f3e25f9
Perk7 <denisperkov0@gmail.com>
2022-04-08 19:19:59 +0700

    new

--- database.py
+++ database.py
@@ -1,15 +1,17 @@
-import sqlite3
 import json
 import datetime
+import psycopg2 as ps
 
 class BotDB:
-    def __init__(self, dbname="bot.sqlite"):
+    def __init__(self, dbname="d3u1kuoi5v2vjf"):
         self.dbname = dbname
-        self.conn = sqlite3.connect(dbname, check_same_thread=False)
+        self.conn = ps.connect(dbname=dbname,
+                               user='jxjqwavtrskxma', password='7da6c906fd65f5c337e5f88f1168b30a6c9cd1fd04052570486d711febbca39c', host='ec2-52-18-116-67.eu-west-1.compute.amazonaws.com', port='5432')
 
     def setup(self):
         stmt = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, data TEXT)"
-        self.conn.execute(stmt)
+        cursor = self.conn.cursor()
+        cursor.execute(stmt)
         self.conn.commit()
 
     def add_user(self, obj):
@@ -18,36 +20,47 @@ class BotDB:
             'exerc': obj['exerc'],
             }
         
-        stmt = "INSERT INTO users (id, data) VALUES (?, ?)"
+        stmt = "INSERT INTO users (id, data) VALUES (%s, %s)"
         args = (obj['id'], json.dumps(data))
-        self.conn.execute(stmt, args)
+        cursor = self.conn.cursor()
+        cursor.execute(stmt, args)
         self.conn.commit()
     
     def is_user(self, id_user):
-        stmt = "SELECT id FROM users WHERE id = (?)"
-        args = (id_user,) 
-        print(self.conn.execute(stmt, args).fetchall())
-        return bool(self.conn.execute(stmt, args).fetchall())
+        stmt = "SELECT id FROM users WHERE id = (%s)"
+        args = (id_user,)
+        cursor = self.conn.cursor()
+        cursor.execute(stmt, args)
+        
+        return bool(cursor.fetchall())
 
     def update_user(self, id_user, obj):
-        stmt = "UPDATE users SET data = (?) WHERE id = (?)"
+        stmt = "UPDATE users SET data = (%s) WHERE id = (%s)"
         args = (json.dumps(obj), id_user)
-        self.conn.execute(stmt, args)
+        cursor = self.conn.cursor()
+        cursor.execute(stmt, args)
+        
         self.conn.commit()
     
     def get_user(self, id_user):
-        stmt = "SELECT data FROM users WHERE id = (?)"
+        stmt = "SELECT data FROM users WHERE id = (%s)"
         args = (id_user, )
-        data = self.conn.execute(stmt, args).fetchall()[0][0]
+        cursor = self.conn.cursor()
+        data = cursor.fetchall()[0][0]
+        
         return json.loads(data)
 
     def delete_user(self, id_user):
-        stmt = "DELETE FROM users WHERE id = (?)"
+        stmt = "DELETE FROM users WHERE id = (%s)"
         args = (id_user, )
-        self.conn.execute(stmt, args)
+        cursor = self.conn.cursor()
+        cursor.execute(stmt, args)
+        
         self.conn.commit()
     
     def delete_all(self):
         stmt = 'DELETE FROM users'
-        self.conn.execute(stmt)
+        cursor = self.conn.cursor()
+        cursor.execute(stmt)
+        
         self.conn.commit()
